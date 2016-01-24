2a6a5d5a62c78e2e1de2035deaecd51b5e384924
Dennis Bailey <bailey.dennis@gmail.com>
2016-01-24 13:16:38 -0700

    search for a movie keyword

--- main.js
+++ main.js
@@ -1,21 +1,17 @@
 $(document).on('ready', function() {
   console.log('READY, PLAYER ONE?');
   
-  function getRepos() {
-    var i = 1;
-    var repoArray = [];
-    var shouldContinue = true;
-    while (true) {
-      $.get("https://api.github.com/users/dennisbailey/repos?page="+i).done(function(response) {
-        if(response.length < 30) {
-          shouldContinue = false;
-        } else { 
-          i++;
-        } 
-        repoArray.push(response);
-      });
-    }
-    return repoArray;   
-  } 
-  console.log(getRepos()); 
-});
+  // Accept a title keyword and search based on that keyword
+    $('form').on('submit', function(e){
+      e.preventDefault();
+      
+      var keyword = $('input').val();
+      
+      var url = 'http://www.omdbapi.com/?s='+keyword+'&r=json'
+      $.get(url).done(function(res) {
+    
+      console.log(res);
+      $('input').val('');
+    });     
+  });
+}); 
