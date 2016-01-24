76c8fb9e20ffca70b4b98a1fa5e757b1e946aeff
Dennis Bailey <bailey.dennis@gmail.com>
2016-01-24 12:48:30 -0700

    Switiching APIs due to rate limiting

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
 $(document).on('ready', function() {
-  console.log('READY PLAYER ONE?');
+  console.log('READY, PLAYER ONE?');
+  
+  function getRepos() {
+    var i = 1;
+    var repoArray = [];
+    var shouldContinue = true;
+    while (true) {
+      $.get("https://api.github.com/users/dennisbailey/repos?page="+i).done(function(response) {
+        if(response.length < 30) {
+          shouldContinue = false;
+        } else { 
+          i++;
+        } 
+        repoArray.push(response);
+      });
+    }
+    return repoArray;   
+  } 
+  console.log(getRepos()); 
 });
