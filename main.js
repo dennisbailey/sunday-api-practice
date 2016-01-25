c9fe20ec9b3809a2af0fa015d5e62a3e4bbca173
Dennis Bailey <bailey.dennis@gmail.com>
2016-01-25 08:13:37 -0700

    adds Michael's example based on our walkthrough

--- main.js
+++ main.js
@@ -4,14 +4,59 @@ $(document).on('ready', function() {
   // Accept a title keyword and search based on that keyword
     $('form').on('submit', function(e){
       e.preventDefault();
-      
+      $('#results').empty();
       var keyword = $('input').val();
-      
       var url = 'http://www.omdbapi.com/?s='+keyword+'&r=json'
-      $.get(url).done(function(res) {
+      
+      
+      function getMovies() {
+        $.get(url).done(function(res) {
     
-      console.log(res);
+        res.Search.forEach(function(obj){
+        $('body').append('<img src="'+obj.poster+'">');
+      });
+      
+      // Reset the input field
       $('input').val('');
-    });     
-  });
+        });     
+      });
 }); 
+
+// Michael's
+$(document).ready(function() {
+
+  $('#filter-btn').hide();
+
+  $('form').on('submit', function(e){
+    e.preventDefault();
+    $('#results').empty();
+    var keyword = $('input').val();
+    getMovies(keyword);
+  });
+
+  function getMovies(keyword) {
+    var url = 'http://www.omdbapi.com/?s='+keyword+'&r=json';
+    $.get(url)
+    .done(function(res) {
+      res.Search.forEach(function(obj){
+        // $('#results').append('<img src="'+obj.Poster+'">');
+        $('#results').append('<li>'+obj.Title+'&nbsp;-&nbsp;'+obj.Year+'</li>');
+        $('#filter-btn').show();
+      });
+      $('input').val('');
+    });
+  }
+
+    /*
+    On button click...
+      1. get all movies from the dom
+      2. isolate the year
+      3. logic to find if year >= 2000
+      4. add those movies back to the dom
+    */
+    $('#filter-btn').on('click', function(){
+      var test = $('li').val('');
+      console.log(test[0].innerText);
+    });
+
+});
